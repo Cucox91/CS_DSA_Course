784f2dbebc72b55fb48867cb8b86b71423ea04a7
Raziel Arias <raziel.arias1991@gmail.com>
2025-06-14 18:01:30 -0400

    Graphs (Missing Adjacent Matrix)

---
+++ Graphs/AdjacentListGraph.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+
+namespace Graphs;
+
+public class GraphNode
+{
+    public int Value { get; set; }
+    public List<int>? Adjacent { get; set; }
+}
+
+public class AdjacentListGraph
+{
+    public long NumberOfNodes { get; set; } = 0;
+    public Hashtable AdjacentList { get; set; } = new Hashtable();
+
+    public void AddVertex(int value)
+    {
+        if (AdjacentList[value] == null)
+        {
+            AdjacentList.Add(value, new List<int>());
+            NumberOfNodes++;
+        }
+        {
+            Console.WriteLine("Element Already Exists");
+        }
+    }
+
+    public void AddEdge(int node1, int node2)
+    {
+        var vertex1 = AdjacentList[node1];
+        var vertex2 = AdjacentList[node2];
+
+        if (vertex1 == null)
+        {
+            Console.WriteLine($"Vertex with value: {node1} doesn't exist");
+        }
+        if (vertex2 == null)
+        {
+            Console.WriteLine($"Vertex with value: {node2} doesn't exist");
+        }
+
+        if (vertex1 != null && vertex2 != null)
+        {
+            var listVertex1 = (List<int>)AdjacentList[node1]!;
+            var listVertex2 = (List<int>)AdjacentList[node2]!;
+            if (listVertex1.Contains(node2)) // We only check in one because we add and remove at the same time from both.
+            {
+                Console.WriteLine($"Relationship between {node1} and {node2} exists.");
+            }
+            else
+            {
+                listVertex1.Add(node2);
+                listVertex2.Add(node1);
+            }
+        }
+    }
+
+    public void ShowConnections()
+    {
+        var keys = AdjacentList.Keys.Cast<int>().Order();
+        foreach (var key in keys)
+        {
+            var adjacentNodes = (List<int>)AdjacentList[key]!;
+            var nodes = "";
+            foreach (var value in adjacentNodes)
+            {
+                nodes += value.ToString() + " ";
+            }
+            Console.WriteLine($"{key}-->{nodes}");
+        }
+    }
+
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,49 +1,32 @@
-﻿using Trees;
+﻿using Graphs;
 
-BST myBst = new BST();
+AdjacentListGraph myALG = new AdjacentListGraph();
 
-myBst.Insert(50);
-myBst.Insert(30);
-myBst.Insert(70);
-myBst.Insert(20);
-myBst.Insert(40);
-myBst.Insert(60);
-myBst.Insert(80);
-myBst.Insert(10);
-myBst.Insert(35);
-myBst.Insert(65);
+myALG.AddVertex(0);
+myALG.AddVertex(1);
+myALG.AddVertex(2);
+myALG.AddVertex(3);
+myALG.AddVertex(4);
+myALG.AddVertex(5);
+myALG.AddVertex(6);
 
-Console.WriteLine("Pre Order");
-myBst.TraversePreOrder();
+myALG.AddEdge(3, 1);
+myALG.AddEdge(3, 4);
+myALG.AddEdge(4, 2);
+myALG.AddEdge(4, 5);
+myALG.AddEdge(1, 2);
+myALG.AddEdge(1, 0);
+myALG.AddEdge(0, 2);
+myALG.AddEdge(6, 5);
 
-Console.WriteLine("In Order");
-myBst.TraverseInOrder();
+myALG.ShowConnections();
 
-Console.WriteLine("Post Order");
-myBst.TraversePostOrder();
-
-// Console.WriteLine("Find " + myBst.LookupNode(50).Value);
-// Console.WriteLine("Find " + myBst.LookupNode(60).Value);
-// Console.WriteLine("Find " + myBst.LookupNode(80).Value);
-
-// var result = myBst.LookupNode(100);
-// var stringVal = result != null ? result.Value.ToString() : "NULL";
-// Console.WriteLine("Find " + stringVal);
-
-
-
-myBst.Remove(10);
-myBst.Remove(30);
-myBst.Remove(50);
-
-Console.WriteLine("After Removals:");
-
-Console.WriteLine("Pre Order");
-myBst.TraversePreOrder();
-
-Console.WriteLine("In Order");
-myBst.TraverseInOrder();
-
-Console.WriteLine("Post Order");
-myBst.TraversePostOrder();
-
+/*
+Answer:
+0--> 1 2
+1--> 3 2 0
+2--> 4 1 0
+3--> 1 4
+5--> 4 5
+6--> 5
+*/
